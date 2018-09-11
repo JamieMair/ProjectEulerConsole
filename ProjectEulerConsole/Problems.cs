f718834eb1fc1e7511e623832399a78c0b8216ce
Jamie Mair <jamie.mair@hotmail.co.uk>
2018-09-11 17:01:36 +0100

    Initial Commit

---
+++ ProjectEulerConsole/Problems.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace ProjectEulerConsole
+{
+    public static class Problems
+    {
+        #region DynamicMethod
+
+        public static string DoProblem(int problemNumber, object optionalParam = null)
+        {
+            //Gets the method of the problem to solve.
+            MethodInfo getMethod = (typeof(Problems)).GetMethod($"Problem{problemNumber}Answer");
+
+
+            //Sees if the method is correct and then tries to solve it.
+            if (getMethod != null)
+            {
+                Stopwatch sw = new Stopwatch();
+
+                sw.Start();
+                object result;
+                if (getMethod.GetParameters().Length == 1 && optionalParam != null)
+                {
+                    if (optionalParam is long)
+                        result = getMethod.Invoke(null, new object[] { long.Parse(optionalParam.ToString()) });
+                    else if (optionalParam is int)
+                        result = getMethod.Invoke(null, new object[] { int.Parse(optionalParam.ToString()) });
+                    else
+                        result = getMethod.Invoke(null, new object[] { optionalParam.ToString() });
+                }
+                else if (optionalParam is object[] && getMethod.GetParameters().Length == (optionalParam as object[]).Length)
+                    result = getMethod.Invoke(null, optionalParam as object[]);
+                else if (optionalParam == null)
+                {
+                    object[] emptyParameters = new object[getMethod.GetParameters().Length];
+                    for (int i = 0; i < emptyParameters.Length; i++)
+                    {
+                        emptyParameters[i] = Type.Missing;
+                    }
+                    result = getMethod.Invoke(null, BindingFlags.OptionalParamBinding |
+            BindingFlags.InvokeMethod |
+            BindingFlags.CreateInstance,
+            null,
+            emptyParameters,
+            CultureInfo.InvariantCulture);
+                }
+                else
+                {
+                    result = null;
+                }
+
+                sw.Stop();
+                if (result != null)
+                    return result.ToString() + $" ({sw.ElapsedMilliseconds.ToString()}ms)";
+                else
+                    return "You entered the incorrect parameters!";
+            }
+
+
+            return "Problem number not recognised.";
+        }
+
+        #endregion
+    }
+}
--- ProjectEulerConsole/Program.cs
+++ ProjectEulerConsole/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProjectEulerConsole
 {
@@ -10,6 +8,85 @@ namespace ProjectEulerConsole
     {
         static void Main(string[] args)
         {
+            Console.SetWindowSize(150, 25);
+            start:
+
+
+            Console.Clear();
+            Console.WriteLine("Enter the problem number you wish to solve: ");
+            string input = Console.ReadLine();
+
+            bool IsnInt = false;
+            bool IsnLong = false;
+            bool IsnStr = false;
+            long n = 0;
+            int nInt = 0;
+            string[] arguments = input.Split(',');
+            if (arguments.Length == 2)
+            {
+                if (int.TryParse(arguments[1], out nInt))
+                {
+                    IsnInt = true;
+                }
+                else if (long.TryParse(arguments[1], out n))
+                {
+                    IsnLong = true;
+                }
+                else
+                {
+                    IsnStr = true;
+                }
+            }
+
+            int problemNum;
+            if (int.TryParse(arguments[0], out problemNum))
+            {
+                string result;
+                if (IsnInt)
+                    result = (Problems.DoProblem(problemNum, nInt));
+                else if (IsnLong)
+                    result = (Problems.DoProblem(problemNum, n));
+                else if (IsnStr)
+                    result = (Problems.DoProblem(problemNum, arguments[1]));
+                else if (arguments.Length > 2)
+                {
+                    object[] myParameters = new object[arguments.Length - 1];
+                    for (int i = 1; i < arguments.Length; i++)
+                    {
+                        if (int.TryParse(arguments[i], out int intResult))
+                            myParameters[i - 1] = intResult;
+                        else if (long.TryParse(arguments[i], out long longResult))
+                            myParameters[i - 1] = longResult;
+                        else
+                            myParameters[i - 1] = arguments[i];
+                    }
+                    result = (Problems.DoProblem(problemNum, myParameters));
+                }
+                else
+                    result = (Problems.DoProblem(problemNum));
+
+                Console.WriteLine(result);
+                using (StreamWriter writer = new StreamWriter(Environment.CurrentDirectory + "/outputlog.txt", true))
+                {
+                    writer.WriteLine(input + ": " + result);
+                }
+            }
+            else
+            {
+                Console.WriteLine("You did not enter a valid number.");
+            }
+
+            Console.WriteLine("Press e to exit or any other key do another problem.");
+            if (Console.ReadKey().Key == ConsoleKey.E)
+                goto end;
+            else
+                goto start;
+
+            end:
+            { }
+
         }
+
+
     }
 }
